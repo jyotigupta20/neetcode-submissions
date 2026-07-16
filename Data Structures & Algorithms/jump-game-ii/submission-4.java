2424397af795fe9534b1a27b09edf3d8edbf3ae9
jyotigupta20 <132284955+jyotigupta20@users.noreply.github.com>
2026-07-16 21:46:17 +0530

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        if(nums.length <= 1) {
+           return 0;
+        }
+
+       int maxReach = 0;
+       int currReach = 0;
+       int jumpCount = 0;
+
+        for(int i=0; i < nums.length; i++) {
+                
+
+            if(i > maxReach) {
+                return 0;
+            }
+
+            maxReach = Math.max(maxReach, i+nums[i]);
+
+            if(i == currReach) {
+                jumpCount++;
+                currReach = maxReach;
+                if(maxReach >= nums.length-1) {
+                    return jumpCount;
+                }
+            }
+        }
+
+        return jumpCount; 
+    }
+}
