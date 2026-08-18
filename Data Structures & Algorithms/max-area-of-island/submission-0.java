fcef548bb3fb4383c94c1204c601fce667d34547
jyotigupta20 <132284955+jyotigupta20@users.noreply.github.com>
2026-08-18 22:47:28 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+    final int[][] direction = {{0,-1}, {0, 1}, {-1,0},{1,0}};
+    public int maxAreaOfIsland(int[][] grid) {
+        if(grid == null || grid.length==0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
+        
+        for(int i = 0; i< row; i++){
+            for(int j = 0; j<col; j++){
+                if(grid[i][j] == 1) {
+                    count = Math.max(count, bfs(grid, i, j));
+                    
+                }
+            }
+        }
+        return count;
+    }
+
+    int bfs(int[][] grid, int currRow, int currCol) {
+        Queue<int[]> queue = new LinkedList();
+        grid[currRow][currCol] = 0;
+        int result = 1; // count curr node as well, since it reaches here it means already it has 1
+        queue.offer(new int[]{currRow, currCol});
+
+        while(!queue.isEmpty()){
+            int[] currNode = queue.poll();
+            int row = currNode[0];
+            int col = currNode[1];
+            for(int[] corr: direction) {
+                int newRow = corr[0] + row;
+                int newCol = corr[1] + col;
+                if(newRow >= 0 && newCol >= 0 
+                && newRow < grid.length 
+                && newCol < grid[0].length
+                && grid[newRow][newCol] == 1){
+                    queue.offer(new int[]{newRow, newCol});
+                    grid[newRow][newCol] = 0;
+                    result++;
+                } 
+            }
+        }
+        return result;
+    }
+}
