18af9a862343de4cbfb60a5e54a5faee9a227f11
jyotigupta20 <132284955+jyotigupta20@users.noreply.github.com>
2026-08-31 16:12:47 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0 || amount < 0) {
+            return -1;
+        }
+        int dp[] = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+
+        for(int i = 1; i <= amount; i++) {
+            for(int j=0; j < coins.length; j++) {
+                if(coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+                }
+            }
+        }
+        if(dp[amount] > amount) {
+            return -1;
+        } else {
+            return dp[amount];
+        }
+    }
+}
